420632500f34e0a44d544c95ac22815193b27e5e
dubtar <dubtar@yandex.ru>
2024-12-19 12:41:27 +0300

    19 cleanup

--- 19/task19.py
+++ 19/task19.py
@@ -1,16 +1,10 @@
-from collections import defaultdict
 from io import TextIOWrapper
 from pathlib import Path
-import sys
 
-Coords = tuple[int, int]
-Directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-sys.setrecursionlimit(150000)
 
 def walk(
     line: str,
-    first_letters: dict[str, list[str]],
+    towels: list[str],
     start: int,
     cache: dict[str, int],
 ) -> int:
@@ -19,32 +13,24 @@ def walk(
     remline = line[start:]
     if remline in cache:
         return cache[remline]
-    result = 0
-    for t in first_letters[line[start]]:
-        if t == line[start:start+len(t)]:
-            result += walk(line, first_letters, start+len(t), cache)
-
+    result = sum(walk(line, towels, start + len(t), cache) for t in towels if t == line[start : start + len(t)])
     cache[remline] = result
     return result
 
 
-
 def main(f: TextIOWrapper, *, verbose: bool = False) -> int:
-    towels = next(f).strip().split(',')
-    first_letters: dict[str, list[str]] = defaultdict(list)
-    for t in towels:
-        t = t.strip()
-        first_letters[t[0]].append(t)
+    towels = [s.strip() for s in next(f).split(',')]
+    next(f)
     result1, result2 = 0, 0
     cache = {}
-    for _, line in enumerate(f):
-        if line.strip():
-            r = walk(line.strip(), first_letters, 0, cache)
+    for line in f:
+        if line := line.strip():
+            r = walk(line.strip(), towels, 0, cache)
             if r > 0:
                 result1 += 1
                 result2 += r
-            print(line.strip(), r)
-
+            if verbose:
+                print(line, r)  # noqa: T201
 
     return result1, result2
 
